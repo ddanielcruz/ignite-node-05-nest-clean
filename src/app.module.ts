045b79832c30c960490dbb826127c18bcd825baa
Daniel Cruz <danielcunha54@gmail.com>
2024-05-08 19:34:55 -0300

    create env module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 
 import { CreateAccountController } from './controllers/create-account.controller'
+import { EnvModule } from './env/env.module'
 import { PrismaService } from './prisma/prisma.service'
 
 @Module({
-  imports: [],
+  imports: [EnvModule],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
